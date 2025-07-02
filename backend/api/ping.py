85b46dcfd3e4793b505f383c0d7b059165080587
Anishka Singh <anishkasinghclass9@gmail.com>
2025-07-02 17:17:45 +0530

    Implement Singleton MongoDB handler and connect to local askage DB (#6)

--- backend/api/ping.py
+++ backend/api/ping.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from mongo_handler import db
 
 title: str = "ping"
 blueprint: Blueprint = Blueprint(title, title)
@@ -6,7 +7,17 @@ blueprint: Blueprint = Blueprint(title, title)
 @blueprint.get("/ping/")
 def endpoint():
     """
-    Sample endpoint to ping backend server.
+    Checks backend and MongoDB connection.
     """
-    
-    return "", 204
+    try:
+        collections = db.list_collection_names()
+        return jsonify({
+            "message": "Pong!",
+            "mongo_collections": collections
+        }), 200
+
+    except Exception as e:
+        return jsonify({
+            "message": "MongoDB connection failed",
+            "error": str(e)
+        }), 500
