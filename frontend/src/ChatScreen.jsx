e34dc862b27024691e7fd1080e14e00156bd2160
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-07-02 13:14:24 +0530

    Made Chrome Extension Chat Screen

--- frontend/src/ChatScreen.jsx
+++ frontend/src/ChatScreen.jsx
@@ -2,15 +2,13 @@ import "./ChatScreen.css";
 import Heading from "./Heading";
 import MessageBox from "./MessageBox";
 import ChatHistory from "./ChatHistory";
-import SendButton from "./SendButton";
 
 function ChatScreen() {
   return (
     <>
       <Heading />
-      <MessageBox />
-      <SendButton />
       <ChatHistory />
+      <MessageBox />
     </>
   );
 }
--- frontend/src/MessageBox.jsx
+++ frontend/src/MessageBox.jsx
@@ -1,13 +1,15 @@
 import "./MessageBox.css";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
 
 function MessageBox() {
   return (
     <>
       <div id="message">
-        <textarea
-          id="message-box"
-          placeholder="Enter your message here..."
-        ></textarea>
+        <textarea id="message-box" placeholder="Ask"></textarea>
+        <Button variant="contained" endIcon={<SendIcon />}>
+          Send
+        </Button>
       </div>
     </>
   );
