ba3feaba17fdf10485e7dffaab49c4962bed2603
Deepak Soni <deepaksonii@outlook.in>
2025-07-04 00:49:19 +0530

    small fix

--- frontend/src/Popup.jsx
+++ frontend/src/Popup.jsx
@@ -8,7 +8,7 @@ function App() {
   const [authState, setAuthState] = useState(null);
 
   useEffect(() => {
-    setAuthState("true");  // Display Login Screen (No JS Logic for now)
+    setAuthState("false");  // Display Login Screen (No JS Logic for now)
   }, []);
 
   return (
