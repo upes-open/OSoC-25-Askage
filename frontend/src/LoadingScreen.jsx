b71c718cb6f34f2a9d7d1ea4aa80a2c626816b85
Arpit Kaushik <arpitkaushikgithub@gmail.com>
2025-07-04 01:30:51 +0530

    Fix: Resolves #29: ArpitKaushik2025/Loading Screen UI

--- frontend/src/LoadingScreen.jsx
+++ frontend/src/LoadingScreen.jsx
@@ -3,7 +3,10 @@ import "./LoadingScreen.css";
 function LoadingScreen({ authState }) {
   return (
     <div id="loading-screen" style={{ display: (authState === "loading") ? "flex" : "none" }}>
-      add loading screen code here...
+      <div id="loader"></div>
+      <div id="loader-screen-info">
+        ASKAGE is loading. Please wait....
+      </div>
     </div>
   );
 }
