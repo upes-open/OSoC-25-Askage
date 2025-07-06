1a406aadb77a6ae5f7e406cdad0bfebb46b1f16c
Deepak Soni <deepaksonii@outlook.in>
2025-07-06 16:22:58 +0530

    post PR code refactor & cleanup

--- backend/utils/rate_limit_handler.py
+++ backend/utils/rate_limit_handler.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from flask_limiter.errors import RateLimitExceeded
 
-def register_error_handlers(app):
+def register_rate_limit_handler(app):
     @app.errorhandler(RateLimitExceeded)
     def handle_rate_limit_error(e):
         return jsonify({
