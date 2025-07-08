a152739c63d3b76f112d02656b5e33e81b2b0a50
DeshwalX <deshwal.abhijeet@outlook.com>
2025-07-08 14:03:06 +0530

    Clear new_conversation logic from db_handler to follow issue scope

--- backend/utils/db_handler.py
+++ backend/utils/db_handler.py
@@ -68,12 +68,10 @@ class MongoHandler:
         Creates a new conversation in Database.
         Returns: conversation_id
         """
-        try:
-            collection = self.db["conversations"]
-            result = collection.insert_one({"user_id": user_id})
-            return str(result.inserted_id)
-        except errors.PyMongoError as e:
-            raise Exception(f"MongoDB error: {e}")
+        
+         # Use `self.db`
+
+        # TODO: Must return conversation id (that's the _id of document just created)
 
     
     def verify_auth_token(self, user_id: ObjectId, session_token: str) -> bool:
